087727143f11dc4a0bb7575154734e64fbbf2fa6
Sergei Shilovsky <sshilovsky@gmail.com>
2014-11-26 20:41:04 +0300

    haulers remember their target to be able to loot

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 /* SPAWNING functions */
+var _ = require('lodash');
 
 var ROLES = {
     "harvester": [Game.WORK, Game.WORK, Game.WORK, Game.MOVE],
@@ -79,14 +80,33 @@ function harvester(creep, source) {
 function hauler(creep, base) {
     if (creep.energy < creep.energyCapacity)
     {
-        target = creep.pos.findNearest(Game.DROPPED_ENERGY);
-        if(!target) return;
-        creep.moveTo(target);
-        creep.pickup(target);
-        return;
-    } 
-    creep.moveTo(base);
-    creep.transferEnergy(base); 
+        var target_xy = creep.memory.target_xy;
+        var target;
+        if(target_xy !== undefined) {
+            var objects = creep.room.lookAt(target_xy[0], target_xy[1]);
+            objects = _.filter(objects, {"type": "energy"});
+            if(objects.length) {
+                target = objects[0].energy;
+            }
+        }
+        if(target !== undefined && target.pos === undefined) {
+            target = undefined;
+        }
+        if(!target) {
+            target = creep.pos.findNearest(Game.DROPPED_ENERGY); // TODO queue           
+        }
+        if(target) {
+            creep.memory.target_xy = [target.pos.x, target.pos.y];
+            creep.moveTo(target);
+            creep.pickup(target);
+        } else {
+            creep.memory.target_xy = undefined;
+        }
+    } else {
+        creep.moveTo(base);
+        creep.transferEnergy(base);
+        creep.memory.target_xy = undefined;
+    }
 }
 /* MAIN */
 
